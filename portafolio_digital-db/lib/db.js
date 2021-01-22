edafa28d513e36df2b8ced2d3df1953071710a26
JUANCMS <montillasanchezjuancarlos@gmail.com>
2021-01-22 01:46:16 -0400

    problem whit connect in database

--- portafolio_digital-db/lib/db.js
+++ portafolio_digital-db/lib/db.js
@@ -21,29 +21,32 @@ class Db {
     this.db = options || defaults.db
   }
 
-  //  objeto de conexion
+  //  objeto de conexion (revisar, error de conexión)
   connect (callback) {
     this.connection = r.connect({
-      host : this.host,
+      host: this.host,
       port: this.port
     })
 
-    //  primero para conectarnos a la base de datos y necesitamos hacer referencia a la conexion y tambien el nombre de la base de datos
-    let db = this.db
-    let connection = this.connection
+    //  le asignamos la propiedad en el método de la conexión
+    this.connected = true
 
-    let setup = co.wrap(function * () {
-      let conn = yield connection
+    //  primero para conectarnos a la base de datos y necesitamos hacer referencia a la conexion y tambien el nombre de la base de datos
+    const db = this.db
+    const connection = this.connection
+
+    const setup = co.wrap(function * () {
+      const conn = yield connection
 
       // luego de estar conectado necesito tener la lista de bases de datos que tengo para eso creamos una variable 'dbList' que espera a que se resuelva la promesa 'r.dbList (es para listar las db que tengo)' . 'llamamos al metodo run pasandole como parametro la conexion' y el run es lo mismo que si fuera en la parte visual del dataexplore y es necesario utilizarlo cuando utilicemos rethinkdb dentro de nodejs
-      let dbList = yield r.dbList().run(conn)
+      const dbList = yield r.dbList().run(conn)
       if (dbList.indexOf(db) === -1) {
         //  para crear la base de datos, necesitamos el metodo 'dbCreate' que vemos en el dataexplorer y le pasamos el nombre de la base de datos como parametro y la referencia a la conexion (run(conn))
         yield r.dbCreate(db).run(conn)
       }
 
       //  realizaremos el mismo paso pero ahora con las tablas, necesitamos buscar la base de datos con el nombre creado y luego listar las tablas de esa db y conectarnos
-      let dbTables = yield r.db(db).tableList().run(conn)
+      const dbTables = yield r.db(db).tableList().run(conn)
       if (dbTables.indexOf('images') === -1) {
         yield r.db(db).tableCreate('images').run(conn)
       }
@@ -59,6 +62,18 @@ class Db {
 
     //  esta sola linea indica que si no le pasan callback retorno la promesa resuelta de lo contrario manejo todo lo anterior como una funcion asincrona con caconst
     return Promise.resolve(setup()).asCallback(callback)
+  }
+
+  //  creamos un método llamado 'disconnect' que recibe un argumento (callback) y me una condición que indica si la conexión no esta realizada, me retorne una nueva promesa con el error
+  disconnect (callback) {
+    if (!this.connected) {
+      return Promise.reject(new Error('No se ha conectado')).asCallback(callback)
+    }
+    this.connected = false
+    return Promise.resolve(this.connection).then((conn) => conn.close())
+  }
+
+  saveImage (image, callback) {
   }
 }
 
--- portafolio_digital-db/test/db-test.js
+++ portafolio_digital-db/test/db-test.js
@@ -1,8 +1,35 @@
+//  Prueba para utilizar los test de la subida de imagen y base de datos
 'use strict'
 
 const test = require('ava')
 const uuid = require('uuid-base62')
-const Db = require('../index.js')
+const r = require('rethinkdb')
+const Db = require('../')
 
+//  nos permite generar una base de datos de prueba con el nombre de portafolio pero con id diferentes
 const dbName = `portfolio-digital_${uuid.v4()}`
-const db = new Db({db: dbName})
+
+//  instanciamos la clase pasándole como objeto la options del nombre de la base de datos
+const db = new Db({ db: dbName })
+
+// debemos configurar la base de datos cada vez que la vayamos a modificar como prueba y necesitamos esperar a conectarnos.
+test.before('configuracion database', async t => {
+  await db.connect()
+  t.true(db.connected, 'conectado a la base de datos')
+})
+
+test.after('desconectarme database', async t => {
+  await db.disconnect()
+  t.false(db.connected, 'desconectado database')
+})
+
+test.after.always('limpiar la base de datos', async t => {
+  const conn = await r.connect({})
+  await r.dbDrop(dbName).run(conn)
+})
+
+//  creamos el test para guarda imágenes
+test('guardar imagen', async t => {
+  //  vamos a garantizar que la clase tenga la funcion 'saveImage' guardar imagen. 'is' me permite hacer una comparación de que lo que está en el primer valor sea igual al segundo (derecha)
+  t.is(typeof db.saveImage, 'function', 'guardando imagen...')
+})
